efff97d62a1fa4e13f53b712f9a780c8f3006aa1
Sagen Hansda <sagenhansda19072003@gmail.com>
2024-12-03 21:01:20 +0530

    correct code B

--- Codeforces_Round_990_(Div.2)/_B_Replace_Character.cpp
+++ Codeforces_Round_990_(Div.2)/_B_Replace_Character.cpp
@@ -4,19 +4,29 @@ using namespace std;
 void solve() {
 	int n; cin >> n;
 	string s; cin >> s;
-	unordered_map<char, int> f;
-	for(char c : s) f[c]++;
-	char maxc = s[0];
-	for(const auto& [k, v] : f) {
-		if(v > f[maxc]) maxc = k;
-	}
+	unordered_map<char, int> freq;
+	for(char c : s) freq[c]++;
+	char maxChar = s[0];
+    char minFreqChar = 0;
+    int minFreq = INT_MAX;
+	for (const auto& [k, v] : freq) {
+        if (v > freq[maxChar]) maxChar = k;
+    }
 
-	for(int i = 0; i < n; i++) {
-		if(s[i] != maxc) {
-			s[i] = maxc;
-			break;
-		}
-	}
+	for (const auto& [k, v] : freq) {
+        if (k != maxChar && v < minFreq) {
+            minFreq = v;
+            minFreqChar = k;
+        }
+    }
+
+    if (!minFreqChar) minFreqChar = maxChar;
+	for (char& c : s) {
+        if (c == minFreqChar) {
+            c = maxChar;
+            break;
+        }
+    }
 	cout << s << endl;
 }
 
