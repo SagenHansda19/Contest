e9ef9dff5c78f5c97a738903a10343cd2846808e
Sagen Hansda <sagenhansda19072003@gmail.com>
2025-01-11 19:19:43 +0530

    kya ho raha hai gucchi

---
+++ AtCoder_Beginner_Contest_388/A.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() {
+    string s;
+    cin >> s;
+    cout << s[0] << "UPC";
+}
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    solve();
+    return 0;
+}
+
